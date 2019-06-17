f445baf29ac8bf7fd76d563da2d305e1d76a045a
aminography <amin.maxim@gmail.com>
2019-06-17 12:28:32 +0430

    Improving localizations.

--- sample-app/src/test/java/com/aminography/primecalendar/sample/ExampleUnitTest.kt
+++ sample-app/src/test/java/com/aminography/primecalendar/sample/ExampleUnitTest.kt
@@ -1,9 +1,7 @@
 package com.aminography.primecalendar.sample
 
-import com.aminography.primecalendar.persian.PersianCalendar
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,16 +13,6 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun aaaaaa() {
-        val persian = PersianCalendar(Locale.ENGLISH)
-        persian.set(1398, 2, 24)
-        println(persian.longDateString)
-
-        persian.set(Calendar.MONTH, 8)
-        println(persian.longDateString)
     }
 
 }
