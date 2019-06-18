4419db2bf53777b8da7a4cf196443942e10d4d9c
aminography <amin.maxim@gmail.com>
2019-06-18 14:15:09 +0430

    Improving localizations.

--- library/src/main/java/com/aminography/primecalendar/common/CalendarFactory.kt
+++ library/src/main/java/com/aminography/primecalendar/common/CalendarFactory.kt
@@ -4,6 +4,7 @@ import com.aminography.primecalendar.PrimeCalendar
 import com.aminography.primecalendar.civil.CivilCalendar
 import com.aminography.primecalendar.hijri.HijriCalendar
 import com.aminography.primecalendar.persian.PersianCalendar
+import java.util.*
 
 /**
  * A factory class which creates calendar objects based on desired type.
@@ -21,6 +22,15 @@ object CalendarFactory {
             CalendarType.CIVIL -> CivilCalendar()
             CalendarType.PERSIAN -> PersianCalendar()
             CalendarType.HIJRI -> HijriCalendar()
+        }
+    }
+
+    @JvmStatic
+    fun newInstance(type: CalendarType, locale: Locale): PrimeCalendar {
+        return when (type) {
+            CalendarType.CIVIL -> CivilCalendar(locale)
+            CalendarType.PERSIAN -> PersianCalendar(locale)
+            CalendarType.HIJRI -> HijriCalendar(locale)
         }
     }
 
