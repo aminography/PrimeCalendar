ee0e950b83656b4a24de313a095e3c86910bbd95
Amin <amin.maxim@gmail.com>
2019-11-02 21:02:29 +0330

    Japanese calendar is added.

--- java-sample-app/src/test/java/com/aminography/primecalendar/sample/ExampleUnitTest.java
+++ java-sample-app/src/test/java/com/aminography/primecalendar/sample/ExampleUnitTest.java
@@ -1,15 +1,6 @@
 package com.aminography.primecalendar.sample;
 
-import com.aminography.primecalendar.civil.CivilCalendar;
-import com.aminography.primecalendar.common.CalendarFactory;
-import com.aminography.primecalendar.common.CalendarType;
-import com.aminography.primecalendar.hijri.HijriCalendar;
-import com.aminography.primecalendar.persian.PersianCalendar;
-
 import org.junit.Test;
-
-import java.util.Locale;
-import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,16 +15,5 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void aaaaa() {
-//        PersianCalendar calendar = (PersianCalendar) CalendarFactory.newInstance(CalendarType.PERSIAN);
-        PersianCalendar calendar = new PersianCalendar(new Locale("fa"));
-//        PersianCalendar calendar = new PersianCalendar(TimeZone.getDefault());
-        PersianCalendar persian = calendar.toPersian();
-        HijriCalendar hijri = calendar.toHijri();
-        CivilCalendar civil = calendar.toCivil();
-    }
-
 
 }
