b0e99d1d6d806d037473d765c9c4371338f78d8f
M.Amin Hassani <amin.hassani@cafebazaar.cloud>
2020-11-30 13:21:30 +0330

    Improving arabic digits

--- library/src/main/java/com/aminography/primecalendar/common/Utils.kt
+++ library/src/main/java/com/aminography/primecalendar/common/Utils.kt
@@ -1,5 +1,7 @@
 package com.aminography.primecalendar.common
 
+import com.aminography.primecalendar.hijri.HijriCalendar
+import com.aminography.primecalendar.persian.PersianCalendar
 import java.util.*
 
 /**
@@ -7,31 +9,33 @@ import java.util.*
  */
 
 internal const val delimiter = "/"
-internal const val FA = "fa"
-internal const val AR = "ar"
 
 internal fun normalize(locale: Locale, number: Int): String = when (locale.language) {
-    FA -> (if (number <= 9) "0$number" else "$number").withPersianDigits
-    AR -> (if (number <= 9) "0$number" else "$number").withArabicDigits
+    PersianCalendar.DEFAULT_LOCALE -> (if (number <= 9) "0$number" else "$number").withPersianDigits
+    HijriCalendar.DEFAULT_LOCALE -> (if (number <= 9) "0$number" else "$number").withArabicDigits
     else -> if (number <= 9) "0$number" else "$number"
 }
 
 internal fun comma(locale: Locale): String = when (locale.language) {
-    FA -> "،"
-    AR -> "،"
+    PersianCalendar.DEFAULT_LOCALE -> "،"
+    HijriCalendar.DEFAULT_LOCALE -> "،"
     else -> ","
 }
 
-internal fun localizeNumber(locale: Locale, number: Int): String = when (locale.language) {
-    FA -> number.withPersianDigits
-    AR -> number.withArabicDigits
-    else -> "$number"
-}
+fun Number.localizeDigits(locale: Locale): String =
+    "$this".localizeDigits(locale)
 
-internal val Number.withPersianDigits: String
+fun String.localizeDigits(locale: Locale): String =
+    when (locale.language) {
+        PersianCalendar.DEFAULT_LOCALE -> withPersianDigits
+        HijriCalendar.DEFAULT_LOCALE -> withArabicDigits
+        else -> this
+    }
+
+val Number.withPersianDigits: String
     get() = "$this".withPersianDigits
 
-internal val String.withPersianDigits: String
+val String.withPersianDigits: String
     get() = StringBuilder().also { builder ->
         toCharArray().forEach {
             builder.append(
@@ -44,10 +48,10 @@ internal val String.withPersianDigits: String
         }
     }.toString()
 
-internal val Number.withArabicDigits: String
+val Number.withArabicDigits: String
     get() = "$this".withArabicDigits
 
-internal val String.withArabicDigits: String
+val String.withArabicDigits: String
     get() = StringBuilder().also { builder ->
         toCharArray().forEach {
             builder.append(
